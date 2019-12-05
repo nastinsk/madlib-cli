cb15dae9494d811f24e8f926f4ad3d8e92be3eac
nastinsk <nastinsk@gmail.com>
2019-12-04 23:25:13 -0800

    README updated

--- madlib.py
+++ madlib.py
@@ -1,8 +1,8 @@
 # import ReGex module
 import re
 
-# function to take data from template_madlib file
 def read_file(file):
+    """F() to take data from template_madlib file"""
     with open(file, 'r') as f:
         template = (f.read())
         return(template)
@@ -11,28 +11,28 @@ def read_file(file):
 template_file = read_file('template_madlib.txt')
 
 
-
 def prompts_list(pattern, template):
-    # run regex pattern over the template and extract all words to array prompts
+    """F()to run regex pattern over the template and extract all words to array prompts"""
     prompts = re.findall(pattern, template)
     return prompts
 
 def empty_template(pattern, template):
-    # extract all {words} from the template
+    """F() to extract all {words} from the template using pattern"""
     template = re.sub(pattern, "{}", template)
     return template
 
 
-# print prompts to user/return array of users inputs
 def print_prompts():
+    """Print prompts to user and return array of users inputs"""
     user_words = []
     for el in prompts_list("\{[a-zA-Z0-9' -]*\}", template_file):
         user_word = input(f'Give me {el[1:-1]}: -----> ')
         user_words.append(user_word)
     return user_words
 
-# function to output welcome message and rules of the game to the screen
+
 def handle_IO():
+    """F() to output welcome message and rules of the game to the screen"""
 
     # ask if user wants to play
     if input("\n***************************************\n**** Welcome to the Mad Libs Game! ****\n \n Mad Libs is a phrasal template word game\n where user(you) need to input different\n words following prompts. At the end you\n will get a story that was created\n using your inputs.\n \nPlease type 'y' if you want to play:\n") == "y":
@@ -53,8 +53,8 @@ def handle_IO():
 
 
 
-#function to write results to the file
 def write_file(file, contents):
+    """F() to write program results to the file"""
     with open(file, 'w') as f:
         template = (f.write(contents))
 
