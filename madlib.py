db287ad65286585173d5c1114056dedace904167
nastinsk <nastinsk@gmail.com>
2019-12-04 22:46:30 -0800

    game complited

--- madlib.py
+++ madlib.py
@@ -1,29 +1,16 @@
+# import ReGex module
 import re
 
-
-
 # function to take data from template_madlib file
-
-
 def read_file(file):
     with open(file, 'r') as f:
         template = (f.read())
         return(template)
 
-
-# print(read_file('template_madlib.txt'))
-
-# function to save data from template_madlib
-
-# function to extract {arguments} from the template to the array:
-
-
-# print(template)
-
-
-
 # call read_file() to read the template_madlib file
-template = read_file('template_madlib.txt')
+template_file = read_file('template_madlib.txt')
+
+
 
 def prompts_list(pattern, template):
     # run regex pattern over the template and extract all words to array prompts
@@ -36,27 +23,41 @@ def empty_template(pattern, template):
     return template
 
 
-
-
-empty_template("\{[a-zA-Z0-9' -]*\}", template)
-
+# print prompts to user/return array of users inputs
 def print_prompts():
     user_words = []
-    for el in prompts_list("\{[a-zA-Z0-9' -]*\}", template):
+    for el in prompts_list("\{[a-zA-Z0-9' -]*\}", template_file):
         user_word = input(f'Give me {el[1:-1]}: -----> ')
         user_words.append(user_word)
     return user_words
 
 # function to output welcome message and rules of the game to the screen
-if input("**** Welcome to the Mad Libs Game! ****\n \n Mad Libs is a phrasal template word game\n where user(you) need to input different\n words following prompts. At the end you\n will get a story that was created\n using your inputs.\n Please type 'y' if you want to play\n") == "y":
+def handle_IO():
 
-    print(print_prompts())
+    # ask if user wants to play
+    if input("\n***************************************\n**** Welcome to the Mad Libs Game! ****\n \n Mad Libs is a phrasal template word game\n where user(you) need to input different\n words following prompts. At the end you\n will get a story that was created\n using your inputs.\n \nPlease type 'y' if you want to play:\n") == "y":
+
+        # store return from print_promts()
+        user_words = print_prompts()
+
+        # store the empty template in new var
+        template = empty_template("\{[a-zA-Z0-9' -]*\}", template_file)
+
+        # return template formated with users inputs
+        formatted_template = f'\nHere is your story:\n-------------------\n{template.format(*user_words)[22:]}'
+
+        # print results to user
+        print(formatted_template)
+
+        return formatted_template
 
 
 
+#function to write results to the file
+def write_file(file, contents):
+    with open(file, 'w') as f:
+        template = (f.write(contents))
 
-
-# function to prompt user to asking for inputs for different words
-# function to populate our template with collected user inputs
-# fucntion to provide response to user
 # write the completed template to the new file
+write_file('madlib_result.txt', handle_IO())
+
